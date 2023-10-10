5ff80ee92f8140cc50cc13680e3cc369e0cf0356
Bijoy1001 <bijoykhan1001@gmail.com>
2023-10-11 03:00:21 +0600

    tailwindcss setup and json data create

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,16 @@
+const Card = () => {
+    return (
+        <div>
+            <div>
+            <h1 className='text-center font-bold text-5xl text-transparent bg-clip-text bg-gradient-to-r from-pink-400 to-purple-600'>Programming Hero</h1>
+            <h3 className="text-center font-bold text-3xl pt-4">Course Registration</h3>
+            </div>
+          
+            <div>
+                
+            </div>
+        </div>
+    );
+};
+
+export default Card;
